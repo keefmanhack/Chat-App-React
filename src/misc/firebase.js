720b784f8629d55c386ddfae75523a28f888fa1e
Keefer Gregoire <keefergregoire@Keefers-MacBook-Pro.local>
2021-05-23 14:57:26 -0400

    added firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyBy7pqMAcR7oRLnbqa1LSBlrEL52oXIP9Q",
+    authDomain: "chat-app-c0e27.firebaseapp.com",
+    projectId: "chat-app-c0e27",
+    storageBucket: "chat-app-c0e27.appspot.com",
+    messagingSenderId: "72330048291",
+    appId: "1:72330048291:web:112f2f3bf6ca116768d296"
+}
+
+const app = firebase.initializeApp(config);
